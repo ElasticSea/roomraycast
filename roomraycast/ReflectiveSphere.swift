07b3c478f6516e411a5c8d0ffc7d0981aeb5244c
Elastic Sea <developer@elasticsea.com>
2026-07-22 10:30:52 -0700

    Add pure reflection material

--- roomraycast/ReflectiveSphere.swift
+++ roomraycast/ReflectiveSphere.swift
@@ -5,9 +5,17 @@
 
 import simd
 
+struct PureReflectionMaterial: Sendable, Equatable {
+    let reflectivity: Float = 1
+    let roughness: Float = 0
+    let metallic: Float = 1
+    let diffuseContribution: Float = 0
+}
+
 struct ReflectiveSphere: Sendable {
     static let diameter: Float = 0.25
     static let radius: Float = diameter / 2
 
     var originFromSphereTransform: matrix_float4x4?
+    var material = PureReflectionMaterial()
 }
